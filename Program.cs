0b0b62f249b4cd2db458165939d10bbeffbb4ab1
ionizy <ionizy@gmail.com>
2023-05-20 20:46:26 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -37,38 +37,50 @@
 // }
 // _____________Задание 3__________________
 
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num==1)
+// {
+//     Console.WriteLine("Monday");
+// }
+// else if (num==2)
+// {
+//     Console.WriteLine("Tuesday");
+// }
+// else if (num==3)
+// {
+//     Console.WriteLine("Wednesday");
+// }
+// else if (num==4)
+// {
+//     Console.WriteLine("Thursday");
+// }
+// else if (num==5)
+// {
+//     Console.WriteLine("Frieday");
+// }
+
+// else if (num==6)
+// {
+//     Console.WriteLine("Saturday");
+// }
+// else if (num==7)
+// {
+//     Console.WriteLine("Sunday");
+// }
+// else 
+// {
+//     Console.WriteLine("Error");
+// }
+//___________________Задание 4________________________
+
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num==1)
-{
-    Console.WriteLine("Monday");
-}
-else if (num==2)
-{
-    Console.WriteLine("Tuesday");
-}
-else if (num==3)
-{
-    Console.WriteLine("Wednesday");
-}
-else if (num==4)
-{
-    Console.WriteLine("Thursday");
-}
-else if (num==5)
-{
-    Console.WriteLine("Frieday");
-}
 
-else if (num==6)
+int negNum = -1*num;
+
+while (negNum <= num)
 {
-    Console.WriteLine("Saturday");
-}
-else if (num==7)
-{
-    Console.WriteLine("Sunday");
-}
-else 
-{
-    Console.WriteLine("Error");
+    Console.WriteLine(negNum);
+    negNum = negNum + 1;
 }
