92ea2946be9512932db87af666bb3e4ff2b28046
ionizy <ionizy@gmail.com>
2023-05-25 04:02:10 +0300

    comlete hw

--- Program.cs
+++ Program.cs
@@ -196,21 +196,26 @@
 // }
 //__________________ЗАДАНИЕ 4________________
 
-// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
+// Console.WriteLine("Введите число");
 
-Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int evenNum = 2;
+// Console.Write ("Четные числа: ");
 
-int num = Convert.ToInt32(Console.ReadLine());
+// while (evenNum<=num)
+// {
+//     if ((evenNum==num) || (evenNum==num-1))
+//    {
+//         Console.Write(evenNum + ".");
+//         evenNum=evenNum+2;
+//    } 
+//     else
+//     {
+//         Console.Write(evenNum + ", ");
+//         evenNum=evenNum+2;
+//     }
+// }       
+    
 
-int num1=1;
 
-Console.WriteLine("Четные числа");
-while (num1 <= num)
-{
-    if (num1 % 2 == 0)
-    {
-        
-    }
-}
+
